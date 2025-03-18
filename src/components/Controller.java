377f92ae88dcc0c772f1b99f9e9010f8e3cd50e9
Krish Pillai <pillai@lycoming.edu>
2025-03-18 15:37:35 -0400

    Controller updated.

--- src/components/Controller.java
+++ src/components/Controller.java
@@ -3,36 +3,46 @@ package components;
 import views.ControllerView;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.image.ImageObserver;
+import java.awt.event.*;
 
-public class Controller implements ActionListener, ImageObserver {
-
-    private PolymeraseChainReactor pcr;
+public class Controller extends MouseAdapter implements ActionListener, ItemListener {
+    private ControllerView view;
 
     public Controller() {
-        ControllerView controllerView = new ControllerView(this);
+        view = new ControllerView(this);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        //TODO
-        JMenuItem source = (JMenuItem)(e.getSource());
-        System.out.println(source.getText());
+        JMenuItem item = (JMenuItem) e.getSource();
+        System.out.println("Selected " + item.getText());
+    }
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+
     }
 
     @Override
-    public boolean imageUpdate(Image img,
-                               int infoflags,
-                               int x, int y, int width,
-                               int height) {
-        return false;
+    public void mouseReleased(MouseEvent e) {
+        int x = e.getXOnScreen();
+        int y = e.getYOnScreen();
+        int ret = e.getModifiers();
+        if ( (ret & InputEvent.BUTTON3_MASK)
+                == InputEvent.BUTTON3_MASK) {
+            System.out.println("Button 3 pressed");
+            JPopupMenu popupMenu = new JPopupMenu();
+            popupMenu.setLocation(x, y);
+            popupMenu.add(new JLabel("Hey!"));
+            popupMenu.setVisible(true);
+        }
+
+
+        super.mouseReleased(e);
     }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(Controller::new);
     }
+
 
 }
